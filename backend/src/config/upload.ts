800f8087b68925a32f947524eece07f3f87b6fcb
Pedro Henrique <pedro.henrique@helpper.com.br>
2020-10-15 01:56:05 -0300

    feature - Criado as views de images e orphanages, desenvolvido visualização de imagens por url, instalado o cors, criado validção para campos do POST e tratamento de exceções.

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -4,7 +4,7 @@ import path from 'path';
 export default {
     storage: multer.diskStorage({
         destination: path.join(__dirname, '..', '..', 'uploads'),
-        filename: (req, file, cb) => {
+        filename: (request, file, cb) => {
             const fileName = `${Date.now()}-${file.originalname}`;
 
             cb(null, fileName);
--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,27 +1,35 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
+
 import Orphanage from '../models/Orphanage';
 
+import orphanagesView from '../views/orphanages_view';
+
 export default {
-    async index(req: Request, res: Response) {
+    async index(request: Request, response: Response) {
         const orphanagesRepository = getRepository(Orphanage);
 
-        const orphanages = await orphanagesRepository.find();
+        const orphanages = await orphanagesRepository.find({
+            relations: ['images'],
+        });
 
-        return res.json({data: orphanages})
+        return response.json(orphanagesView.renderMany(orphanages));
     },
 
-    async show(req: Request, res: Response) {
-        const { id } = req.params;
+    async show(request: Request, response: Response) {
+        const { id } = request.params;
 
         const orphanagesRepository = getRepository(Orphanage);
 
-        const orphanage = await orphanagesRepository.findOneOrFail(id);
+        const orphanage = await orphanagesRepository.findOneOrFail(id, {
+            relations: ['images'],
+        });
 
-        return res.json({data: orphanage})
+        return response.json(orphanagesView.render(orphanage));
     },
 
-    async create(req: Request, res: Response) {
+    async create(request: Request, response: Response) {
         const {
             name,
             latitude,
@@ -30,16 +38,16 @@ export default {
             instructions,
             opening_hours,
             open_on_weekends
-        } = req.body;
+        } = request.body;
     
         const orphanagesRepository = getRepository(Orphanage);
     
-        const requestImages = req.files as Express.Multer.File[];
+        const requestImages = request.files as Express.Multer.File[];
         const images = requestImages.map(image => {
             return { path: image.filename };
         })
 
-        const orphanage = orphanagesRepository.create({
+        const data = {
             name,
             latitude,
             longitude,
@@ -48,10 +56,30 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+        };
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(Yup.object().shape({
+                path: Yup.string().required(),
+                })
+            ),
         });
+
+        await schema.validate(data, {
+            abortEarly: false,
+        });
+
+        const orphanage = orphanagesRepository.create(data);
     
         await orphanagesRepository.save(orphanage);
         
-        return res.status(201).json({data: orphanage});
+        return response.status(201).json(orphanage);
     }
 }
